54772bf82a31d9c37413c2901146f35135ccedf5
wangyi <chunhei2008@qq.com>
2016-12-20 13:31:24 +0800

    fixed refresh token

--- src/Core/AuthorizerAccessToken.php
+++ src/Core/AuthorizerAccessToken.php
@@ -10,7 +10,6 @@
 
 namespace Chunhei2008\EasyOpenWechat\Core;
 
-use Chunhei2008\EasyOpenWechat\Contracts\AuthorizerRefreshTokenContract;
 use Chunhei2008\EasyOpenWechat\Support\Log;
 use Doctrine\Common\Cache\Cache;
 use EasyWeChat\Core\AccessToken;
@@ -60,7 +59,7 @@ class AuthorizerAccessToken extends AccessToken
     protected $prefix = 'easyopenwechat.core.authorizer_access_token.';
 
 
-    public function __construct($componentAppId, $authorizerAppId = '', AuthorizerRefreshTokenContract $authorizerRefreshToken, ComponentAccessToken $componentAccessToken, Cache $cache = null)
+    public function __construct($componentAppId, $authorizerAppId = '', AbstractAuthorizerRefreshToken $authorizerRefreshToken, ComponentAccessToken $componentAccessToken, Cache $cache = null)
     {
         $this->componentAppId         = $componentAppId;
         $this->authorizerAppId        = $this->appId = $authorizerAppId;
